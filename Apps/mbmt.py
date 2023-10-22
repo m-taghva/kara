d8ffb38870c2e15188306a4f95592b4e5e84a54f
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-10-22 13:45:20 +0330

    Update mbmt.py

--- Apps/mbmt.py
+++ Apps/mbmt.py
@@ -41,7 +41,7 @@ def main(argv):
     # Check if the subprocess has finished
     if run_process.returncode == 0:
        # Run another Python script after workloadgen.py has finished
-       merge = ["python3", "csv-merger.py", "./../result,all_hosts_output.csv"] 
+       merge = ["python3", "./../status/csv-merger.py", "./../result,all_hosts_output.csv"] 
        subprocess.run(merge, check=True)
 
 if __name__ == "__main__":
