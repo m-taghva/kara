2026811f61de5e1727e01e7a2934bbe1f7fb4c57
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-11-26 13:26:26 +0330

    Update status_reporter.py

--- Status/status_reporter.py
+++ Status/status_reporter.py
@@ -12,9 +12,9 @@ RESET = "\033[0m"
 YELLOW = "\033[1;33m"
 
 ##### Constants #####
-START_TIME_SUM = 10 # increase your test start time 
-END_TIME_SUBTRACT = 10 # decrease your test end time
-TIME_GROUP = 10 # time group for query 2
+START_TIME_SUM = 10  # increase your test start time
+END_TIME_SUBTRACT = 10  # decrease your test end time
+TIME_GROUP = 10  # time group for query 2
 CONFIG_FILE = "./../conf/Status-reporter/status.conf"
 ##### End of Constants #####
 
@@ -42,7 +42,7 @@ with open(CONFIG_FILE, "r") as stream:
 print("")
 print(f"{YELLOW}========================================{RESET}")
 print("")
-# Parse command-line arguments 
+# Parse command-line arguments
 parser = argparse.ArgumentParser(description="Influxdb Status-reporter")
 parser.add_argument("-t", "--time", help="Time range in the format 'start_time,end_time' (e.g., 'Y-M-D h:m:s,Y-M-D h:m:s')")
 parser.add_argument("-m", "--metric_file", help="Comma-separated list of metric file paths")
@@ -58,10 +58,10 @@ os.makedirs(OUTPUT_PARENT_DIR, exist_ok=True)
 
 # Check if the time option is provided
 if args.time:
-   time_range = args.time
+    time_range = args.time
 else:
-   # Read time range from the configuration file
-   time_range = data_loaded.get('time', [])[0]
+    # Read time range from the configuration file
+    time_range = data_loaded.get('time', [])[0]
 
 # Generate CSV file name based on the time range
 time_range_parts = time_range.split(',')
@@ -70,7 +70,7 @@ start_time_csv = start_time.replace(" ", "-")
 end_time_csv = end_time.replace(" ", "-")
 output_csv = os.path.join(OUTPUT_PARENT_DIR, f"{start_time_csv}_{end_time_csv}.csv")
 if os.path.exists(output_csv):
-   os.remove(output_csv)
+    os.remove(output_csv)
 
 metric_operation_mapping = {}
 metric_operations = []
@@ -86,43 +86,49 @@ else:
         metric_operations.append(metric_operation)
         metric_files_config = [metric_file.get('path', '')]
         metric_operation_mapping[metric_file.get('path', '')] = metric_operation
-        
-output_csv_str = ["Host_alias"] # name of first column in csv
+
+output_csv_str = ["Host_alias"]  # name of the first column in csv
 csvi = 0
 # Loop through each combination of time range, host, IP, PORT, and execute the curl command
-for mc_server, values in data_loaded.get('influxdbs', {}).items():
-    IP = values['ip_influx']
-    PORT = values['port_influx']
-    DATABASE = values['database_influx']
-    HOST = values['host_monster']
-    ALIAS = values['alias_monster'] if values['alias_monster'] and len(values['alias_monster']) > 1 else HOST
-    start_time_utc = convert_tehran_to_utc(start_time, START_TIME_SUM)
-    end_time_utc = convert_tehran_to_utc(end_time, -END_TIME_SUBTRACT)
-    output_csv_str.append(ALIAS)  # value inside first column of csv
-    csvi += 1
-    for metric_file, metric_operation in metric_operation_mapping.items():
-        if os.path.isfile(metric_file):
-            metrics = get_metrics_from_file(metric_file)
-            for metric_name in metrics:
-                metric_name = metric_name.strip()
-                if csvi == 1:
-                   metric_name = metric_name.replace(" ", "")
-                   output_csv_str[0] += f",{metric_operation}_{metric_name.replace('netdata.', '')}"
-                # Construct the curl command for query 1
-                query1_curl_command = f'curl -sG "http://{IP}:{PORT}/query" --data-urlencode "db={DATABASE}" --data-urlencode "q=SELECT {metric_operation}(\\"value\\") FROM \\"{metric_name}\\" WHERE (\\"host\\" =~ /^{HOST}$/) AND time >= \'{start_time_utc}\' AND time <= \'{end_time_utc}\' fill(none)"'
-                query_result = subprocess.getoutput(query1_curl_command)
-                values = json.loads(query_result).get('results', [{}])[0].get('series', [{}])[0].get('values', [])
-                values = [str(v[1]) for v in values]
-                output_csv_str[csvi] += "," + ",".join(values)
-                print(f"{BOLD}Add metrics to CSV, please wait ...{RESET}")
-                # Construct the curl command for query 2
-                query2_curl_command = f'curl -sG "http://{IP}:{PORT}/query" --data-urlencode "db={DATABASE}" --data-urlencode "q=SELECT {metric_operation}(\\"value\\") FROM /{metric_name}/ WHERE (\\"host\\" =~ /^{HOST}$/) AND time >= \'{start_time_utc}\' AND time <= \'{end_time_utc}\' GROUP BY time({TIME_GROUP}s) fill(none)"'
-                query2_output = subprocess.getoutput(query2_curl_command)
-                os.system(f"python3 ./../Status/image_renderer.py '{query2_output}' '{HOST}' '{path_dir}'")
+for mc_server, config in data_loaded.get('influxdbs', {}).items():
+    ip = config.get('ip')
+    for data_key, data_config in config.items():
+        if isinstance(data_config, dict) and 'influx_port' in data_config:
+           influx_port = data_config.get('influx_port')
+           databases = data_config.get('databases', []) 
+           for db_info in databases:
+               for db_name, db_data in db_info.items():
+                   hostls = db_data.get('hostls', [])
+                   for host in hostls:
+                       host_name, alias = host.split(',')
+                       alias = alias if alias and len(alias) > 1 else host_name
+                       start_time_utc = convert_tehran_to_utc(start_time, START_TIME_SUM)
+                       end_time_utc = convert_tehran_to_utc(end_time, -END_TIME_SUBTRACT)
+                       output_csv_str.append(alias)  # value inside the first column of csv
+                       csvi += 1
+                       for metric_file, metric_operation in metric_operation_mapping.items():
+                           if os.path.isfile(metric_file):
+                              metrics = get_metrics_from_file(metric_file)
+                              for metric_name in metrics:
+                                  metric_name = metric_name.strip()
+                                  if csvi == 1:
+                                     metric_name = metric_name.replace(" ", "")
+                                     output_csv_str[0] += f",{metric_operation}_{metric_name.replace('netdata.', '')}"
+                                  # Construct the curl command for query 1
+                                  query1_curl_command = f'curl -sG "http://{ip}:{influx_port}/query" --data-urlencode "db={db_name}" --data-urlencode "q=SELECT {metric_operation}(\\"value\\") FROM \\"{metric_name}\\" WHERE (\\"host\\" =~ /^{host_name}$/) AND time >= \'{start_time_utc}\' AND time <= \'{end_time_utc}\' fill(none)"'
+                                  query_result = subprocess.getoutput(query1_curl_command)
+                                  values = json.loads(query_result).get('results', [{}])[0].get('series', [{}])[0].get('values', [])
+                                  values = [str(v[1]) for v in values]
+                                  output_csv_str[csvi] += "," + ",".join(values)
+                                  print(f"{BOLD}Add metrics to CSV, please wait ...{RESET}")
+                                  # Construct the curl command for query 2
+                                  query2_curl_command = f'curl -sG "http://{ip}:{influx_port}/query" --data-urlencode "db={db_name}" --data-urlencode "q=SELECT {metric_operation}(\\"value\\") FROM /{metric_name}/ WHERE (\\"host\\" =~ /^{host_name}$/) AND time >= \'{start_time_utc}\' AND time <= \'{end_time_utc}\' GROUP BY time({TIME_GROUP}s) fill(none)"'
+                                  query2_output = subprocess.getoutput(query2_curl_command)
+                                  os.system(f"python3 ./../Status/image_renderer.py '{query2_output}' '{host_name}' '{path_dir}'")
 # Write the CSV file for each time range
 with open(output_csv, 'a') as csv_file:
-     for line in output_csv_str:
-         csv_file.write(line + "\n")
+    for line in output_csv_str:
+        csv_file.write(line + "\n")
 print("")
 print(f"{BOLD}Done! Csv and Images are saved in the {RESET}{YELLOW}'{OUTPUT_PARENT_DIR}'{RESET}{BOLD} directory{RESET}")
 print("")
