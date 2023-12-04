89765a8c6bbbbedb9f21c416d56161450b08a7cf
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-12-04 10:20:57 +0330

    Update status_reporter.py

--- Status/status_reporter.py
+++ Status/status_reporter.py
@@ -12,8 +12,8 @@ RESET = "\033[0m"
 YELLOW = "\033[1;33m"
 
 ##### Constants #####
-START_TIME_SUM = 10  # increase your test start time
-END_TIME_SUBTRACT = 10  # decrease your test end time
+START_TIME_SUM = 10  # increase your report start time
+END_TIME_SUBTRACT = 10  # decrease your report end time
 TIME_GROUP = 10  # time group for query 2
 CONFIG_FILE = "./../conf/Status-reporter/status.conf"
 ##### End of Constants #####
