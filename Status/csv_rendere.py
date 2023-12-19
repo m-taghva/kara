39c151f6ccd9d307c499b1070df8ec79f125d74f
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-12-19 08:49:50 +0330

    Update csv_rendere.py

--- Status/csv_rendere.py
+++ Status/csv_rendere.py
@@ -1,23 +1,23 @@
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
+import argparse
+import os
 
 def csv_to_image(csv_file, output_image):
     # Read CSV file into a DataFrame
     df = pd.read_csv(csv_file)
     # Convert DataFrame to an image
     image = create_image_from_dataframe(df)
-    # Save the image
+    # Save the image with the same name as the CSV file
     image.save(output_image)
 
 def create_image_from_dataframe(df):
     # Get the dimensions of the DataFrame
     num_rows, num_cols = df.shape
-
     # Set image properties
-    cell_width = 120
+    cell_width = 150
     cell_height = 20
     font_size = 15
-
     # Create a blank image
     image_width = num_cols * cell_width
     image_height = (num_rows + 1) * cell_height  # +1 for header row
@@ -37,6 +37,20 @@ def create_image_from_dataframe(df):
             draw.text((col * cell_width + 5, (row + 1) * cell_height + 5), str(value), font=font, fill='black')
     return image
 
+def main():
+    # Set up command-line argument parsing
+    parser = argparse.ArgumentParser(description='Convert CSV file to image')
+    parser.add_argument('-c', '--csv', help='Input CSV file', required=True)
+    args = parser.parse_args()
+    # Check if the CSV file exists
+    if not os.path.exists(args.csv):
+        print(f"Error: The specified CSV file '{args.csv}' does not exist.")
+        return
+    # Create the output image file name based on the CSV file name
+    output_image = os.path.splitext(args.csv)[0] + '.png'
+    # Call the function to convert CSV to image
+    csv_to_image(args.csv, output_image)
+    print(f"Image '{output_image}' created successfully.")
+
 if __name__ == "__main__":
-    # Replace 'input.csv' with your CSV file and 'output.png' with the desired image file name
-    csv_to_image('/path/to/csv', 'image.png')
+    main()
