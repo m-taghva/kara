53291eb3b9a1518677fec26a227b1d92d8a44315
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-12-26 10:56:26 +0330

    Update monstaver-restore.py

--- Temporary-restore/monstaver-restore.py
+++ Temporary-restore/monstaver-restore.py
@@ -31,7 +31,7 @@ for mc_server, config in data_loaded.get('influxdbs', {}).items():
     database = config.get('database')
 
     # Ensure the target restore directory exists
-    create_dir_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo mkdir -p {influx_restore_volume}/{directoryname}/dbs/restore/'"
+    create_dir_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -i -u root {container_name} mkdir -p {influx_restore_volume}/{directoryname}/dbs/restore/'"
     create_dir_process = subprocess.run(create_dir_command, shell=True)
     create_dir_exit_code = create_dir_process.returncode
     if create_dir_exit_code == 0:
@@ -41,8 +41,7 @@ for mc_server, config in data_loaded.get('influxdbs', {}).items():
        sys.exit(1)
 
     # Extract the backup.tar.gz
-    extract_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -it -u root {container_name} tar -xf {influx_extracted_data}/{directoryname}/dbs/{container_name}.tar.gz -C {influx_restore_volume}/{directoryname}/dbs/restore/'"
-    print(extract_command)
+    extract_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -i -u root {container_name} tar -xf {influx_extracted_data}/{directoryname}/dbs/{container_name}.tar.gz -C {influx_restore_volume}/{directoryname}/dbs/restore/'"
     extract_process = subprocess.run(extract_command, shell=True)
     exit_code = extract_process.returncode
     if exit_code == 0:
@@ -53,7 +52,7 @@ for mc_server, config in data_loaded.get('influxdbs', {}).items():
        print() 
 
     # Restore on influxdb phase - Ckeck if it is first backup or not - Define the command you want to run
-    check_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -it {container_name} influx -execute 'show databases''"
+    check_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} \"sudo docker exec -i -u root {container_name} influx -execute 'SHOW DATABASES'\""
     try:
        output_bytes = subprocess.check_output(check_command, shell=True)
        output = output_bytes.decode('utf-8')
@@ -64,34 +63,34 @@ for mc_server, config in data_loaded.get('influxdbs', {}).items():
 
     # Print the captured output and check for "opentsdb"
     if output is not None and database in output:
-       restore_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -it {container_name} influxd restore -portable -db {database} -newdb {tempdb_name} {influx_restore_volume}/{directoryname}/dbs/restore/'"
+       restore_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -i {container_name} influxd restore -portable -db {database} -newdb {tempdb_name} {influx_restore_volume}/{directoryname}/dbs/restore/{database}'"
        restore_process = subprocess.run(restore_command, shell=True)
        restore_exit_code = restore_process.returncode
        if restore_exit_code == 1:
-          print("\033[91mRestore failed.\033[0m")
-          print()
+           print("\033[91mRestore failed.\033[0m")
+           print()
       
-          # Merge phase
-          merge_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -it {container_name} influx -execute 'SELECT * INTO \"{database}\".autogen.:MEASUREMENT FROM \"{tempdb_name}\".autogen./.*/ GROUP BY *''"
-          merge_process = subprocess.run(merge_command, shell=True)
-          merge_exit_code = merge_process.returncode
-          if merge_exit_code == 1:
-             print("\033[91mFailure in merging.\033[0m")
-             print()
+           # Merge phase
+           merge_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -i {container_name} influx -execute 'SELECT * INTO \"{database}\".autogen.:MEASUREMENT FROM \"{tempdb_name}\".autogen./.*/ GROUP BY *''"
+           merge_process = subprocess.run(merge_command, shell=True)
+           merge_exit_code = merge_process.returncode
+           if merge_exit_code == 1:
+              print("\033[91mFailure in merging.\033[0m")
+              print()
 
-          # Drop tmp db
-          drop_tmp_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -it {container_name} influx -execute 'drop database {tempdb_name}''"
-          drop_tmp_process = subprocess.run(drop_tmp_command, shell=True)
-          drop_tmp_exit_code = drop_tmp_process.returncode
-          if drop_tmp_exit_code == 1:
-             print(f"\033[91mFailure in dropping {tempdb_name}.\033[0m")
-             print()
-          if restore_exit_code & merge_exit_code & drop_tmp_exit_code == 1:
-             print("\033[92mBackup restored successfully.\033[0m")
-             print()
+           # Drop tmp db
+           drop_tmp_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -i {container_name} influx -execute 'drop database {tempdb_name}''"
+           drop_tmp_process = subprocess.run(drop_tmp_command, shell=True)
+           drop_tmp_exit_code = drop_tmp_process.returncode
+           if drop_tmp_exit_code == 1:
+              print(f"\033[91mFailure in dropping {tempdb_name}.\033[0m")
+              print()
+           if restore_exit_code & merge_exit_code & drop_tmp_exit_code == 1:
+              print("\033[92mBackup restored successfully.\033[0m")
+              print()
 
     elif output is not None and database not in output:
-         restore_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -it {container_name} influxd restore -portable -db {database} {influx_restore_volume}/{directoryname}/dbs/restore/' "
+         restore_command = f"ssh -p {ssh_port} {ssh_user}@{ip_influxdb} 'sudo docker exec -i {container_name} influxd restore -portable -db {database} {influx_restore_volume}/{directoryname}/dbs/restore/{database}' "
          restore_process = subprocess.run(restore_command, shell=True)
          restore_exit_code = restore_process.returncode
          if restore_exit_code == 1:
