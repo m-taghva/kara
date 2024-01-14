af576ab0b5240f3ff622282f9827637b34a0ce3a
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-01-14 09:57:04 +0330

    Update config_gen.py

--- Test_Work_Run/config_gen.py
+++ Test_Work_Run/config_gen.py
@@ -5,41 +5,7 @@ import sys
 import os
 import argparse
 
-# cleanup output dir for new template
-def cleanup_output_config_gen(output_directory):
-    for filename in os.listdir(output_directory):
-        if "#" in filename:
-            file_path = os.path.join(output_directory, filename)
-            os.remove(file_path)
 
-conf_number = 0
-def replace_tags(input_text, conf_name):
-    global conf_number
-    currentTag = re.search("#\d+{", input_text)
-    if currentTag:
-        TagRegex = currentTag.group() + "[^}]+}[^#]*#"
-        for i in range(len(re.search(TagRegex, input_text).group().split('}')[0].split(','))):
-            newInput = input_text
-            tagName = ""
-            for similarTag in re.findall(TagRegex, input_text):
-                similarTagList = similarTag.split('{')[1].split('}')[0].split(',')
-                newInput = re.sub(TagRegex, similarTagList[i], newInput, 1)
-                tagName += ("#" + re.search("(?<=})[^#]*(?=#)", similarTag).group() + ":" + str(similarTagList[i]))
-            replace_tags(newInput, conf_name + tagName)
-    else:
-        conf_number += 1
-        replace_vars(input_text, "{:04}".format(conf_number) + conf_name)
-
-def replace_vars(input_text, conf_name):
-    currentVar = re.search("\?\d+L\d+[sd]", input_text)
-    if currentVar:
-        if currentVar.group()[-1] == 's':
-            replace_vars(input_text.replace(currentVar.group(),str(''.join(random.choices(string.ascii_uppercase + string.digits,k=int(currentVar.group().split('L')[1].split('s')[0]))))),conf_name)
-        else:
-            replace_vars(input_text.replace(currentVar.group(),str(''.join(random.choices(string.digits,k=int(currentVar.group().split('L')[1].split('d')[0]))))),conf_name)
-    else:
-        with open(os.path.join(output_directory, f"{conf_name}#"), 'w') as outfile:
-            outfile.write(input_text)
             
 def main(input_file_path, output_directory):
     if not os.path.exists(output_directory):
@@ -47,7 +13,7 @@ def main(input_file_path, output_directory):
     cleanup_output_config_gen(output_directory)
     with open(input_file_path, 'r') as inputFile:
         input_text = inputFile.read()
-    replace_tags(input_text, "")
+    replace_tags(input_text, "", output_directory)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate configuration files.')
