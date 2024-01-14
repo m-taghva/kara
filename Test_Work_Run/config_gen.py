c3052778ee9cc2988b05be4c0f851c37b6dd1d59
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-01-14 09:22:39 +0330

    Update config_gen.py

--- Test_Work_Run/config_gen.py
+++ Test_Work_Run/config_gen.py
@@ -40,22 +40,20 @@ def replace_vars(input_text, conf_name):
     else:
         with open(os.path.join(output_directory, f"{conf_name}#"), 'w') as outfile:
             outfile.write(input_text)
+            
+def main(input_file_path, output_directory):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    cleanup_output_config_gen(output_directory)
+    with open(input_file_path, 'r') as inputFile:
+        input_text = inputFile.read()
+    replace_tags(input_text, "")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate configuration files.')
     parser.add_argument('-i', '--input', help='Input file path', required=True)
     parser.add_argument('-o', '--output', help='Output directory', required=True)
     args = parser.parse_args()
-
     input_file_path = args.input
     output_directory = args.output
-
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-        
-    # Cleanup the output directory
-    cleanup_output_config_gen(output_directory)
-
-    with open(input_file_path, 'r') as inputFile:
-        input_text = inputFile.read()
-    replace_tags(input_text, "")
+    main(input_file_path, output_directory)
