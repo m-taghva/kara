e1dfc8111ca89c204c1a64d17024c0633ee5a124
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-02-12 15:23:45 +0330

    Update manager_beta.py

--- Manager/manager_beta.py
+++ Manager/manager_beta.py
@@ -47,6 +47,8 @@ def config_gen_agent():
 
 def mrbench_agent(output_subdirs):
     data_loaded = load_config(config_file)
+    all_start_times = []
+    all_end_times = []
     if 'scenario' in data_loaded:
         for task in data_loaded['scenario']:
             if 'Mrbench' in task:
@@ -60,10 +62,12 @@ def mrbench_agent(output_subdirs):
                 mrbench.copy_swift_conf(ring_dir, conf_dir)
                 if one_input_conf:
                    start_time, end_time, result_file_path = mrbench.main(one_input_conf, result_dir)
+                   all_start_times.append(start_time)
+                   all_end_times.append(end_time)
                    if run_status_reporter:
-                      status_reporter_agent(result_file_path, start_time, end_time)
+                      status_reporter.main(result_file_path, start_time, end_time)
                    if run_monstaver:
-                      monstaver.main(time_range=f"{start_time},{end_time}", inputs=[result_file_path], delete=True)
+                      monstaver.main(time_range=f"{start_time},{end_time}", inputs=[result_file_path], delete=True)        
                 else:
                     subdirs = output_subdirs
                     for subdir in subdirs:
@@ -71,23 +75,59 @@ def mrbench_agent(output_subdirs):
                            for test_config in os.listdir(subdir):
                                test_config_path = os.path.join(subdir, test_config)
                                start_time, end_time, result_file_path = mrbench.main(test_config_path, result_dir)
+                               all_start_times.append(start_time)
+                               all_end_times.append(end_time)
                                if run_status_reporter:
-                                  status_reporter_agent(result_file_path, start_time, end_time)  
+                                  status_reporter.main(result_file_path, start_time, end_time)  
                                if run_monstaver:
-                                  monstaver.main(time_range=f"{start_time},{end_time}", inputs=[result_file_path], delete=True)
+                                  monstaver.main(time_range=f"{start_time},{end_time}", inputs=[result_file_path], delete=True)                         
+    else:
+        print("No scenario found in the configuration file.")
+    # Extract first start time and last end time
+    first_start_time = all_start_times[0] 
+    last_end_time = all_end_times[-1] 
+    return first_start_time, last_end_time, result_file_path
+
+def monstaver_backup_agent(first_start_time, last_end_time, result_file_path):
+    data_loaded = load_config(config_file)
+    if 'scenario' in data_loaded:
+        for task in data_loaded['scenario']:
+            if 'Monstaver' in task:
+                config_params = task['Monstaver']
+                operation = config_params.get('operation')
+                batch_mode = config_params.get('batch_mode', False)
+                times_file = config_params.get('time')
+                input_path = config_params.get('input_path')
+                if times_file:
+                   with open(times_file, 'r') as file:
+                        times = file.readlines()
+                        for time_range in times:
+                            start_time, end_time = time_range.strip().split(',')
+                            if input_path:
+                               if operation == "backup":
+                                  monstaver.main(time_range=f"{start_time},{end_time}", inputs=[input_path], delete=True)
+                            elif operation == "restore":
+                                 monstaver.restore()          
+                elif operation == "backup":
+                     if batch_mode:
+                        monstaver.main(time_range=f"{first_start_time},{last_end_time}", inputs=[result_file_path], delete=True)
+                elif operation == "restore":
+                     monstaver.restore()
     else:
         print("No scenario found in the configuration file.")
 
-def status_reporter_agent(result_file_path, start_time, end_time):
-    status_reporter.main(path_dir=result_file_path, time_range=f"{start_time},{end_time}", img=True)
-
-def monstaver_backup_agent(result_file_path, start_time, end_time):
+def status_reporter_agent():
     data_loaded = load_config(config_file)
-    monstaver.main(time_range=f"{start_time},{end_time}", inputs=[result_file_path], delete=True)
+    if 'scenario' in data_loaded:
+        for task in data_loaded['scenario']:
+            if 'Status-Reporter' in task:
+                config_params = task['Status-Reporter']
+                result_dir = config_params.get('output_path')
+                times = config_params.get('times')
 
-def monstaver_restore_agent():
-    data_loaded = load_config(config_file)
-    monstaver.restore()
+    #status_reporter.main(path_dir=result_file_path, time_range=f"{start_time},{end_time}", img=True)
+
+
 
 def analyzer_merge_agent(input_dir, selected_csv):
     data_loaded = load_config(config_file)
@@ -103,10 +143,15 @@ def report_recorder_agent( input_template, output_html, kateb_title):
     subprocess.call(pybot, shell=True)
 
 def main():
+
     output_subdirs = config_gen_agent()
-    mrbench_agent(output_subdirs)
+   
+    first_start_time, last_end_time, result_file_path  = mrbench_agent(output_subdirs)
+   
     #status_reporter_agent(result_file_path, start_time, end_time)
-    #monstaver_backup_agent(result_file_path, start_time, end_time)
+   
+    monstaver_backup_agent(first_start_time, last_end_time, result_file_path)
+
     #monstaver_restore_agent()
     #analyzer_merge_agent(input_parent, csv)
     #time.sleep(10)
