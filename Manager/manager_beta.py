971f0ca21e83d9d36d7ef900d3d68753ba81dc93
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-02-14 09:53:28 +0330

    Update manager_beta.py

--- Manager/manager_beta.py
+++ Manager/manager_beta.py
@@ -4,14 +4,12 @@ import subprocess
 import time
 import yaml
 import argparse
-
 sys.path.append('./../Status_reporter/')
 sys.path.append('./../Monstaver/')
 sys.path.append('./../Analyzer/')
 sys.path.append('./../Config_gen/')
 sys.path.append('./../Report_recorder/')
 sys.path.append('./../Mrbench/')
-
 import mrbench
 import config_gen
 import status_reporter
@@ -65,7 +63,7 @@ def mrbench_agent(output_subdirs):
                    all_start_times.append(start_time)
                    all_end_times.append(end_time)
                    if run_status_reporter:
-                      status_reporter.main(result_file_path, start_time, end_time)
+                      status_reporter.main(path_dir=result_file_path, time_range=f"{start_time},{end_time}", img=True)
                    if run_monstaver:
                       monstaver.main(time_range=f"{start_time},{end_time}", inputs=[result_file_path], delete=True)        
                 else:
@@ -78,7 +76,7 @@ def mrbench_agent(output_subdirs):
                                all_start_times.append(start_time)
                                all_end_times.append(end_time)
                                if run_status_reporter:
-                                  status_reporter.main(result_file_path, start_time, end_time)  
+                                  status_reporter.main(path_dir=result_file_path, time_range=f"{start_time},{end_time}", img=True)  
                                if run_monstaver:
                                   monstaver.main(time_range=f"{start_time},{end_time}", inputs=[result_file_path], delete=True)                         
     else:
@@ -145,38 +143,44 @@ def status_analyzer_agent():
                 analyze = config_params.get('analyze', False)
                 analyze_csv = config_params.get('analyze_csv')
                 transform_dir = config_params.get('transform')
-            if merge:
-                analyzer_merger.main_merge(input_directory=result_dir, selected_csv=merge_csv)
-            if analyze:
-               analyzer_merger.main_analyze(csv_original=result_dir/analyze_csv, transformation_directory=transform_dir)
+                if merge:
+                        analyzer_merger.main_merge(input_directory=result_dir, selected_csv=merge_csv)
+                if analyze:
+                        analyzer_merger.main_analyze(csv_original=f"{result_dir}/{analyze_csv}", transformation_directory=transform_dir)
     else:
         print("No scenario found in the configuration file.")
 
-def report_recorder_agent( input_template, output_html, kateb_title):
+def report_recorder_agent():
     data_loaded = load_config(config_file)
-    pybot = f"python3 ./../../pywikibot/report_recorder.py -it {input_template} -oh {output_html} -kt {kateb_title}"
-    subprocess.call(pybot, shell=True)
+    if 'scenario' in data_loaded:
+        for task in data_loaded['scenario']:
+            if 'Report_Recorder' in task:
+                config_params = task['Report_Recorder']
+                input_template = config_params.get('input_template')
+                output_html = config_params.get('output_html')
+                kateb_title = config_params.get('kateb_title')
+                pybot = f"python3 ./../../pywikibot/report_recorder.py -it {input_template} -oh {output_html} -kt {kateb_title}"
+                subprocess.call(pybot, shell=True)
+    else:
+        print("No scenario found in the configuration file.")
 
 def main():
-
     data_loaded = load_config(config_file)
-
     if data_loaded['scenario']:
        if 'Config_gen':
-            print('test1')
-            output_subdirs = config_gen_agent()
+           output_subdirs = config_gen_agent()
 
     if data_loaded['scenario']:
        if 'Mrbench':
-            first_start_time, last_end_time, result_file_path  = mrbench_agent(output_subdirs)
-   
+           first_start_time, last_end_time, result_file_path  = mrbench_agent(output_subdirs)
+
     if data_loaded['scenario']:
        if 'Status-Reporter':
-            status_reporter_agent()
-       
+           status_reporter_agent()
+
     if data_loaded['scenario']:
        if 'Monstaver':
-            monstaver_backup_agent(first_start_time, last_end_time, result_file_path)
+           monstaver_backup_agent(first_start_time, last_end_time, result_file_path)
 
     if data_loaded['scenario']:
        if 'Status_Analyzer':
@@ -184,7 +188,7 @@ def main():
 
     if data_loaded['scenario']:
        if 'Report_Recorder':
-          report_recorder_agent(input_template, output_html, kateb_title)
+           report_recorder_agent()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='kara tools manager')
