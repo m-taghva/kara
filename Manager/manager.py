179c2229b18faa15a2fa51f55971581f10707f7e
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-02-27 14:40:49 +0330

    Update manager.py

--- Manager/manager.py
+++ Manager/manager.py
@@ -124,25 +124,33 @@ def report_recorder_agent(config_params):
 def main():
     data_loaded = load_config(config_file)
     if 'scenario' in data_loaded:
-        for task in data_loaded['scenario']:
-            if 'Config_gen' in task:
-                config_params = task['Config_gen']
-                output_subdirs = config_gen_agent(config_params)
-            if 'Mrbench' in task:
-                config_params = task['Mrbench']
-                first_start_time, last_end_time = mrbench_agent(config_params, output_subdirs)
-            if 'Status-Reporter' in task:
-                config_params = task['Status-Reporter']
-                status_reporter_agent(config_params)
-            if 'Monstaver' in task:
-                config_params = task['Monstaver']
-                monstaver_agent(config_params, first_start_time, last_end_time)
-            if 'Status_Analyzer' in task:
-                config_params = task['Status_Analyzer']
-                status_analyzer_agent(config_params)
-            if 'Report_Recorder' in task:
-                config_params = task['Report_Recorder']
-                report_recorder_agent(config_params)
+        output_subdirs = None
+        first_start_time = None
+        last_end_time = None
+        for task in data_loaded['scenario']:            
+            try:
+                if 'Config_gen' in task:
+                    config_params = task['Config_gen']
+                    output_subdirs = config_gen_agent(config_params)
+                elif 'Mrbench' in task:
+                      config_params = task['Mrbench']
+                      first_start_time, last_end_time = mrbench_agent(config_params, output_subdirs)
+                elif 'Status-Reporter' in task:
+                      config_params = task['Status-Reporter']
+                      status_reporter_agent(config_params)
+                elif 'Monstaver' in task:
+                      config_params = task['Monstaver']
+                      monstaver_agent(config_params, first_start_time, last_end_time)
+                elif 'Status_Analyzer' in task:
+                      config_params = task['Status_Analyzer']
+                      status_analyzer_agent(config_params)
+                elif 'Report_Recorder' in task:
+                      config_params = task['Report_Recorder']
+                      report_recorder_agent(config_params)
+                else:
+                     print(f"Unknown task: {task}")
+            except Exception as e:
+                 print(f"Error executing task: {task}. Error: {str(e)}")
     else:
         print(f"\033[91mNo scenario found in the configuration file.\033[0m")
 
