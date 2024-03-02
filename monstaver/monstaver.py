88851dcccf23601447233fb6a040755929f69be1
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-03-02 10:07:07 +0330

    Update monstaver.py

--- monstaver/monstaver.py
+++ monstaver/monstaver.py
@@ -8,7 +8,7 @@ import pytz
 import yaml
 from alive_progress import alive_bar
 
-config_file = "./../monstaver/conf/monstaver.conf"
+config_file = "/etc/KARA/monstaver.conf"
 
 def load_config(config_file):
     with open(config_file, "r") as stream:
