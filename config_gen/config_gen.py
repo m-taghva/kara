bf50bd5c1d5e1f897568859a6f3d6eec24bca738
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-03-25 12:09:44 +0330

    Update config_gen.py

--- config_gen/config_gen.py
+++ config_gen/config_gen.py
@@ -11,7 +11,17 @@ def cleanup_output_config_gen(output_directory):
             file_path = os.path.join(output_directory, filename)
             os.remove(file_path)
 
-conf_number = 0
+def replace_vars(input_text, conf_name, output_directory):
+    currentVar = re.search("\?\d+L\d+[sd]", input_text)
+    if currentVar:
+        if currentVar.group()[-1] == 's':
+            replace_vars(input_text.replace(currentVar.group(),str(''.join(random.choices(string.ascii_uppercase + string.digits,k=int(currentVar.group().split('L')[1].split('s')[0]))))),conf_name, output_directory)
+        else:
+            replace_vars(input_text.replace(currentVar.group(),str(''.join(random.choices(string.digits,k=int(currentVar.group().split('L')[1].split('d')[0]))))),conf_name, output_directory)
+    else:
+        with open(os.path.join(output_directory, f"{conf_name}#"), 'w') as outfile:
+            outfile.write(input_text)
+
 def replace_tags(input_text, conf_name, output_directory):
     global conf_number
     currentTag = re.search("#\d+{", input_text)
@@ -28,21 +38,9 @@ def replace_tags(input_text, conf_name, output_directory):
     else:
         conf_number += 1
         replace_vars(input_text, "{:04}".format(conf_number) + conf_name, output_directory)
-
-def replace_vars(input_text, conf_name, output_directory):
-    currentVar = re.search("\?\d+L\d+[sd]", input_text)
-    if currentVar:
-        if currentVar.group()[-1] == 's':
-            replace_vars(input_text.replace(currentVar.group(),str(''.join(random.choices(string.ascii_uppercase + string.digits,k=int(currentVar.group().split('L')[1].split('s')[0]))))),conf_name, output_directory)
-        else:
-            replace_vars(input_text.replace(currentVar.group(),str(''.join(random.choices(string.digits,k=int(currentVar.group().split('L')[1].split('d')[0]))))),conf_name, output_directory)
-    else:
-        with open(os.path.join(output_directory, f"{conf_name}#"), 'w') as outfile:
-            outfile.write(input_text)
                     
-def main(input_file_path, output_directory):
-    global conf_number
-    conf_number = 0
+def main(input_file_path, output_directory, conf_number):
+    conf_number = int(conf_number) if conf_number is not None else 1
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     cleanup_output_config_gen(output_directory)
@@ -54,7 +52,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate configuration files.')
     parser.add_argument('-i', '--input', help='Input file path', required=True)
     parser.add_argument('-o', '--output', help='Output directory', required=True)
+    parser.add_argument('-c', '--conf_number', help='conf_number counter')
     args = parser.parse_args()
-    input_file_path = args.input
-    output_directory = args.output
-    main(input_file_path, output_directory)
+    input_file_path= args.input
+    output_directory=args.output
+    conf_number = int(args.conf_number) if args.conf_number is not None else 1
+    main(input_file_path, output_directory, conf_number)
