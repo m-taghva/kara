635fe0eb2d21e15baf63df200358893e731e0b68
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-05-25 11:41:10 +0330

    Update manager.py

--- manager/manager.py
+++ manager/manager.py
@@ -252,7 +252,7 @@ def mrbench_agent(config_params, config_file, config_output):
                     ring_item = {}
                     for rkey,rvalue in ring_dict.items(): 
                         ring_item[rkey+"_nodes"]=len(set([v.split()[3] for v in rvalue.splitlines()[6:]]))
-                        ring_item.update({"Ring."+rkey+"."+item.split(" ")[1]:item.split(" ")[0] for item in rvalue.splitlines()[1].split(", ")[:5]})
+                        ring_item.update({"Ring."+rkey+"."+item.split(" ")[1]:int(float(item.split(" ")[0])) for item in rvalue.splitlines()[1].split(", ")[:5]})
                     ring_formated = {'ring': ring_item}
                     with open(os.path.join(result_path, 'info.yaml'), 'a') as yaml_file:
                         yaml.dump(ring_formated, yaml_file, default_flow_style=False)
