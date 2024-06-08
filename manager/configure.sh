c04e825fb39fbca4fc268e44cc334fc0380b8229
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-06-08 11:14:00 +0330

    Update configure.sh

--- manager/configure.sh
+++ manager/configure.sh
@@ -65,6 +65,7 @@ fi
 zip_file="$KARA_DIR/KARA/report_recorder/report_recorder_bot.zip"
 zip_destination="$KARA_DIR/KARA/report_recorder/"
 unzip "$zip_file" -d "$zip_destination"
+mv "$KARA_DIR/KARA/report_recorder/report_recorder_bot/*"  "$zip_destination"
 
 #### copy user-config.py to manager dir
 if [ ! -f "$KARA_DIR/KARA/manager/user-config.py" ]; then
