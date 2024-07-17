b7ea38a22470e738338809bb8253db1569ba9cc0
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-07-17 08:26:53 +0330

    Update configure.sh

--- manager/configure.sh
+++ manager/configure.sh
@@ -14,26 +14,26 @@ if [ -z "$PYTHON_DIR" ]; then
     exit 1
 fi
 echo -e "${BOLD}Python directory found: ${RESET}${YELLOW}/usr/local/lib/$PYTHON_DIR${RESET}"
-# Find the "KARA" directory starting from the current directory
+# Find the "kara" directory starting from the current directory
 KARA_DIR=$(dirname "$(pwd)")
 while [ "$KARA_DIR" != "/" ]; do
-    if [ -d "$KARA_DIR/KARA" ]; then
+    if [ -d "$KARA_DIR/kara" ]; then
         break
     fi
     KARA_DIR=$(dirname "$KARA_DIR")
 done
 if [ "$KARA_DIR" == "/" ]; then
-    echo "Error: KARA directory not found"
+    echo "Error: kara directory not found"
     exit 1
 fi
-echo -e "${BOLD}KARA directory found at: ${RESET}${YELLOW}$KARA_DIR/KARA${RESET}"
+echo -e "${BOLD}KARA directory found at: ${RESET}${YELLOW}$KARA_DIR/kara${RESET}"
 # Array of script names
 SCRIPTS=("mrbench" "config_gen" "status_reporter" "monstaver" "analyzer" "report_recorder")
 # Loop through each script and create a symbolic link in the destination directory
 for ((i=0; i<${#SCRIPTS[@]}; i++)); do
     SCRIPT="${SCRIPTS[i]}"
     SCRIPT_DIR="${SCRIPTS[i]}"
-    FULL_SCRIPT_PATH="$KARA_DIR/KARA/$SCRIPT_DIR/$SCRIPT.py"
+    FULL_SCRIPT_PATH="$KARA_DIR/kara/$SCRIPT_DIR/$SCRIPT.py"
     if [ ! -f "$FULL_SCRIPT_PATH" ]; then
         echo -e "${RED}Error: Script $SCRIPT not found in directory $SCRIPT_DIR${RESET}"
         continue
@@ -48,11 +48,11 @@ for ((i=0; i<${#SCRIPTS[@]}; i++)); do
     fi
 done
 
-#### copy config files to /etc/KARA
+#### copy config files to /etc/kara
 # Source directory where your files are located
 source_dir="./sample_configs"
 # Destination directory where you want to move the files
-destination_dir="/etc/KARA"
+destination_dir="/etc/kara"
 mkdir -p "$destination_dir"
 mv "$source_dir"/* "$destination_dir"/
 # Check if files are successfully moved
@@ -63,10 +63,10 @@ else
 fi
 
 #### unzip pywikibot
-zip_file="$KARA_DIR/KARA/report_recorder/report_recorder_bot.zip"
-zip_destination="$KARA_DIR/KARA/report_recorder/"
+zip_file="$KARA_DIR/kara/report_recorder/report_recorder_bot.zip"
+zip_destination="$KARA_DIR/kara/report_recorder/"
 if unzip "$zip_file" -d "$zip_destination"; then
-  if mv "$KARA_DIR/KARA/report_recorder/report_recorder_bot"/* "$zip_destination" > /dev/null 2>&1; then
+  if mv "$KARA_DIR/kara/report_recorder/report_recorder_bot"/* "$zip_destination" > /dev/null 2>&1; then
     echo -e "${YELLOW}Unzip and move report_recoder_bot to /resport_recorder dir successful${RESET}"
   else
     echo -e "${YELLOW}Unzip successful${RESET} ${RED}but move failed for report_recoder_bot${RESET}"
@@ -76,12 +76,12 @@ else
 fi
  
 #### copy user-config.py to manager dir
-if [ ! -f "$KARA_DIR/KARA/manager/user-config.py" ]; then
-    sudo cp -r $KARA_DIR/KARA/report_recorder/user-config.py $KARA_DIR/KARA/manager/
-elif [ -f "$KARA_DIR/KARA/manager/user-config.py" ]; then
-    user_conf_diff=$(diff $KARA_DIR/KARA/manager/user-config.py $KARA_DIR/KARA/report_recorder/user-config.py)
+if [ ! -f "$KARA_DIR/kara/manager/user-config.py" ]; then
+    sudo cp -r $KARA_DIR/kara/report_recorder/user-config.py $KARA_DIR/kara/manager/
+elif [ -f "$KARA_DIR/kara/manager/user-config.py" ]; then
+    user_conf_diff=$(diff $KARA_DIR/kara/manager/user-config.py $KARA_DIR/kara/report_recorder/user-config.py)
     if [ $? -eq 0 ] && [ ! -z "$user_conf_diff" ]; then
-        sudo cp -r $KARA_DIR/KARA/report_recorder/user-config.py $KARA_DIR/KARA/manager/
+        sudo cp -r $KARA_DIR/kara/report_recorder/user-config.py $KARA_DIR/kara/manager/
     fi
 else
     echo -e "${RED}user-config.py is required for run report_recorder${RESET}"
