cc16c1a587eac530f20d4ddc66627e956721af17
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2024-07-31 15:08:29 +0330

    Update status_reporter.py

--- status_reporter/status_reporter.py
+++ status_reporter/status_reporter.py
@@ -76,8 +76,7 @@ def main(metric_file, path_dir, time_range, img=False):
     if log_level is not None:
         log_level_upper = log_level.upper()
         if log_level_upper == "DEBUG" or log_level_upper == "INFO" or log_level_upper == "WARNING" or log_level_upper == "ERROR" or log_level_upper == "CRITICAL":
-            log_dir = f"sudo mkdir {log_path} > /dev/null 2>&1 && sudo chmod -R 777 {log_path}"
-            log_dir_run = subprocess.run(log_dir, shell=True)
+            log_dir_run = subprocess.run(f"sudo mkdir {log_path} > /dev/null 2>&1 && sudo chmod -R 777 {log_path}", shell=True)
             logging.basicConfig(filename= f'{log_path}all.log', level=log_level_upper, format='%(asctime)s - %(levelname)s - %(message)s')
         else:
             print(f"\033[91mInvalid log level:{log_level}\033[0m")  
